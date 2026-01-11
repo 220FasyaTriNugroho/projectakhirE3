757839b0f91181d8cd76bd4f436a2d19802654d6
Akmall10 <akmalprasety53@gmail.com>
2026-01-11 19:41:56 +0700

    setup docker mysql & backend config

--- app/config/database.js
+++ app/config/database.js
@@ -1,14 +1,18 @@
-// TODO: Buat koneksi pool MySQL disini menggunakan Environment Variable (process.env)
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
-module.exports = pool.promise();
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+module.exports = db;
